1c12640cd2710605decb3e6a6f874901d99b158f
Joby Elliott <elhober@unm.edu>
2021-08-30 09:44:03 -0600

    new option to download all signups in one spreadsheet

---
+++ modules/committee_prefs/routes/preference-survey/download-all.php
@@ -0,0 +1,43 @@
+<?php
+
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Digraph\CMS;
+
+$package->cache_noStore();
+
+/** @var Digraph\Modules\committee_prefs\Forms\PreferenceSurvey */
+$survey = $package->noun();
+$writer = WriterEntityFactory::createXLSXWriter();
+$writer->openToBrowser($package->noun()->name() . ' all responses.xlsx');
+$package->makeMediaFile($package->noun()->name() . ' all responses.xlsx');
+
+$headers = [
+    'Name',
+    'Tenured',
+    'College',
+    'Department',
+    'Title',
+    'Max Committees'
+];
+$writer->addRow(
+    WriterEntityFactory::createRowFromArray($headers)
+);
+
+foreach ($survey->allSignups() as $signup) {
+    $row = [
+        $signup->contactInfo()->name(),
+        $signup['contact.tenure'] ? 'Yes' : '',
+        $signup['contact.college'],
+        $signup['contact.department'],
+        $signup['contact.title'],
+        $signup['preferences.max']
+    ];
+    foreach ($signup['preferences.order'] ?? [] as $id) {
+        $row[] = $cms->read($id, false)->name();
+    }
+    $writer->addRow(
+        WriterEntityFactory::createRowFromArray($row)
+    );
+}
+
+$writer->close();
